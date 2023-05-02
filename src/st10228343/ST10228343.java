3e52945bb30250631795b5d8cbc5fb3965c783ed
ST10228343 <108807317+DatTrickster@users.noreply.github.com>
2023-05-02 15:06:00 +0200

    Update ST10228343.java

--- src/st10228343/ST10228343.java
+++ src/st10228343/ST10228343.java
@@ -19,26 +19,33 @@ public class ST10228343 {
         
         //this method is used to run the other methods 
      Login methods = new Login(); 
+       // input method for name
      methods.setName();
+       // input method for surname
      methods.setSurname();
+       //input method for username
      String username=methods.getUsername();
      methods.setUsername(username);
+       //input method for password
      String password=methods.getPassword();
       methods.setPassword(password);
+       // final results method were user would need to re-enter username and password to view results
       methods.confirm();
     }
     
-    
+    // main class 
     public static void main(String[] args) {
          
             String[] options = { "YES", "NO",};
             // gives the option of yes or no to make an account or not if yes then process will start if no then the program will end
     var selection = JOptionPane.showOptionDialog(null, "would you like to create an account ?","", 
                                                       0, 2, null, options, options[0]);
+       // triggers the program to run the start method which hold the input methods 
     if (selection == 0) {
         start();
     }
     if (selection == 1) { 
+       // happens when "NO" was pressed
       JOptionPane.showMessageDialog(null, "Good bye");
     }
 
