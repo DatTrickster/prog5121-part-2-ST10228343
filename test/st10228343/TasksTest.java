a47cca6fdb97cfeeebbef52d011c1c382e028da8
ST10228343 <108807317+DatTrickster@users.noreply.github.com>
2023-06-08 11:57:47 +0200

    Add files via upload

---
+++ test/st10228343/TasksTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package testing.files;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Extinction
+ */
+public class TasksTest {
+    
+    public TasksTest() {
+    }
+
+    @Test
+    public void testEntry() {
+    }
+
+    @Test
+    public void testCheckTaskDescription() {
+    }
+    
+}
