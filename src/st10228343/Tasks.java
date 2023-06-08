18de8c1c9f323ff6c52056bfaee40a638aee8fb8
ST10228343 <108807317+DatTrickster@users.noreply.github.com>
2023-06-08 12:33:10 +0200

    Update Tasks.java

--- src/st10228343/Tasks.java
+++ src/st10228343/Tasks.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package testing.files;
+package st10228343;
 
 import java.util.regex.*;
 import javax.swing.Box;
