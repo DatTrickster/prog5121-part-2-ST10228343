fc1df52bb502f09a075f49c18ba2842f3ae53bdc
ST10228343 <108807317+DatTrickster@users.noreply.github.com>
2023-06-09 14:57:12 +0200

    Update Person.java

--- src/st10228343/Person.java
+++ src/st10228343/Person.java
@@ -8,17 +8,12 @@ package st10228343;
  *
  * @author Extinction
  */
-
-// main object that stores the information needed to push to arraylist
-
 public class Person {
     private String taskName;
     private String description;
     private String time;
     private String status;
     private String devName;
-
-//constructor used for 5he private variables
 
     public Person(String taskName, String description, String time, String status, String devName) {
         this.taskName = taskName;
@@ -27,20 +22,19 @@ public class Person {
         this.status = status;
         this.devName = devName;
     }
-// returns task name
+
     public String getTaskName() {
         return taskName;
     }
-// returns description
+
     public String getDescription() {
         return description;
     }
-//returns the time 
 
     public String getTime() {
         return time;
     }
-// counter to start incrementing the numbe each time new task is added initial 0
+
     private static int currentTaskNumber = 0;
 
     // Other class members and methods...
@@ -49,10 +43,7 @@ public class Person {
         int taskNumber = currentTaskNumber++;
         String taskNamePrefix = taskName.substring(0, Math.min(taskName.length(), 2));
 
- // Extract the surname from the developer's full name
-
-//splits the full name of the dev into name and surname used the last 3 lettera of surname to put into id generation methods
-
+        // Extract the surname from the developer's full name
         String[] fullNameParts = devName.split(" ");
         String developerSurname = "";
         if (fullNameParts.length > 1) {
@@ -61,16 +52,13 @@ public class Person {
         }
 
         StringBuilder displayer = new StringBuilder();
-     // builds the format of the strings  displayer.append(taskNamePrefix).append(":").append(taskNumber).append(":").append(developerSurname);
+        displayer.append(taskNamePrefix).append(":").append(taskNumber).append(":").append(developerSurname);
         return displayer.toString();
     }
 
     public String getStatus() {
-// returns the status done doing ToDo
         return status;
     }
-
-// returns the devs name an surname
 
     public String getDevName() {
         return devName;
